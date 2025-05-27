c1942d663786270f1ddea449c05d08503c500681
Joshua <bataksuhat@gmail.com>
2025-05-28 02:16:22 +0700

    Revert "Merge pull request #19 from G4l1le30/revert-18-rainer"
    This reverts commit 496d38aedaec75d1e45ec656657f0abdb8e1a003, reversing
    changes made to 2e2d6bf3df2be2dbcefd6be7a1e571d4a642aa5a.

--- db.js
+++ db.js
@@ -1,18 +1,18 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE,
-  port: 3306,  // Explicitly specify port
+   host: 'localhost',           
+  user: 'root',               
+  password: '',               
+  database: 'manage_user_fashion_terbaru', 
+  port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  connectTimeout: 10000,  // 10 seconds connection timeout
-  acquireTimeout: 10000,  // 10 seconds pool acquisition timeout
-  enableKeepAlive: true,  // Prevent connection drops
-  keepAliveInitialDelay: 10000  // Keep-alive ping interval
+  connectTimeout: 10000,
+  acquireTimeout: 10000,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 10000
 });
 
 // Test connection immediately
